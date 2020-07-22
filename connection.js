954a83feeb54363a78e58d448011f189549ea115
Hector Jauregui <jhector_1992@yahoo.com>
2020-07-21 22:09:09 -0500

    Settings up to push to heroku

--- connection.js
+++ connection.js
@@ -1,8 +1,11 @@
 //Import the mongoose module
 var mongoose = require('mongoose');
 
+
 //Set up default mongoose connection
-mongoose.connect('mongodb://localhost:27017/markdownApp', { useNewUrlParser: true, useUnifiedTopology: true } );
+//mongoose.connect('mongodb://localhost:27017/markdownApp', { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/markdownApp');
+
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
